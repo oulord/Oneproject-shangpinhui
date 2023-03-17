006fcae5c8856c6c9291a37dc7815d8d0fe22a76
guo <2246338405@qq.com>
2023-03-17 15:29:22 +0800

    第五次提交
    Home 首页拆分静态组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入三级联动组件----注册为全局组件
+import TypeNav from '@/pages/Home/TypeNav'
+// 第一个参数：全局组件的名字 第二个参数：那一个组件
+Vue.component('TypeNav',TypeNav)
+
+// 引入路由
 import router from './router'
 
 Vue.config.productionTip = false
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,12 +1,36 @@
 <template>
     <div>
-        我是首页
+        <!-- 三级联动全局组件：三级联动已经注册为全局组件，因此不需要再引入 -->
+        <TypeNav/>
+        <ListContainer/>
+        <Recommend/>
+        <Rank/>
+        <Like/>
+        <Floor/>
+        <Floor/>
+        <Brand/>
     </div>
 </template>
 
 <script>
+// 引入其余的组件
+import ListContainer from './ListContainer'
+import Recommend from './Recommend'
+import Rank from './Rank'
+import Like from './Like'
+import Floor from './Floor'
+import Brand from './Brand'
+
 export default {
     name: 'Project04Index',
+    components:{
+        ListContainer,
+        Recommend,
+        Rank,
+        Like,
+        Floor,
+        Brand
+    },
 
     data() {
         return {
