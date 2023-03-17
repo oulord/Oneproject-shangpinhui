308041a44533cbc91a67836988218a5b242cdcdc
guo <2246338405@qq.com>
2023-03-17 20:36:08 +0800

    第八次提交
    引入进度条插件：nprogress，使页面在请求数据时页面顶部有特效，建议在请求拦截器和响应拦截器中使用，并可在 nprogress.css 中修改特效的颜色

--- src/api/request.js
+++ src/api/request.js
@@ -1,6 +1,12 @@
 // 对于axios进行二次封装
 import axios from 'axios'
 
+// 引入进度条
+import nprogress from 'nprogress'
+// 在 nprogress 中有两个方法，start()：进度条开始，done()：进度条结束
+// 引入进度条样式
+import "nprogress/nprogress.css"
+
 // 1、利用axios对象的方法 create，去创建axios实例
 const requests = axios.create({
     // 配置对象
@@ -10,12 +16,16 @@ const requests = axios.create({
 
 // 请求拦截器：在请求发出去之前，请求拦截器可以检测到，可以再请求发出去之前做一些事情
 requests.interceptors.request.use((config) => {
+    // 进度条开始
+    nprogress.start()
     // config是一个配置对象，对象里有一个很重要的属性，headers请求头
     return config
 })
 
 // 响应拦截器：
 requests.interceptors.response.use((res) => {
+    // 进度条结束
+    nprogress.done()
     // 成功的回调函数：服务器响应数据回来以后，响应拦截器可以检测到，可以做一些事情
     return res.data
 }, (error) => {
