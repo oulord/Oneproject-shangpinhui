f4ba6fdf7fe29d80739dbec1789dceae84cd66cf
guo <2246338405@qq.com>
2023-04-27 18:47:03 +0800

    第三十四次提交
    进入分页器的操作（为公共组件Pagination）
    完成分页器静态组件

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from './App.vue'
 // 引入三级联动组件----注册为全局组件
 import TypeNav from '@/components/TypeNav'
 import Carousel from '@/components/Carousel'
+import Pagination from '@/components/Pagination'
 // 第一个参数：全局组件的名字 第二个参数：那一个组件
 Vue.component('TypeNav', TypeNav)
 Vue.component('Carousel', Carousel)
+Vue.component('Pagination', Pagination)
 
 // 引入路由
 import router from './router'
