74342376250c27aa8743ad5f2eea42a396b6f5e8
guo <2246338405@qq.com>
2023-05-10 16:45:57 +0800

    第五十次提交
    完成购物车动态展示数据，并进行一些编程逻辑的编写

--- src/store/shopcart.js
+++ src/store/shopcart.js
@@ -1,22 +1,31 @@
 import { reqCartList } from "@/api"
 
 const state = {
-
+    cartList: []
 }
 
 const mutations = {
-
+    GETCARTLIST(state, cartList) {
+        state.cartList = cartList
+    }
 }
 
 const actions = {
     // 获取购物车列表的数据
-    async getCartList() {
+    async getCartList({ commit }) {
         let result = await reqCartList()
+        // 测试是否能获取个人购物车数据
+        // console.log(result);
+        if (result.code == 200) {
+            commit("GETCARTLIST", result.data)
+        }
     }
 }
 
 const getters = {
-
+    cartList(state) {
+        return state.cartList[0] || {}
+    },
 }
 
 export default {
